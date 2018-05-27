f1e1462e6e0661b24482147c7ecdb8e7602642d4
ingprog <zipofar@gmail.com>
2018-05-27 17:23:02 +0300

    add close modal

--- src/components/ModalTaskUpdate.js
+++ src/components/ModalTaskUpdate.js
@@ -1,34 +1,50 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default class ModalTaskUpdate extends React.Component {
+class ModalTaskUpdate extends React.Component {
+
+  closeModal = e => {
+    e.preventDefault();
+    this.props.toggleModalTaskUpdate({ modalState: { state: 'close' } });
+  };
+
+  saveUpdatedTask = id => (values) => {
+    console.log(id) 
+    console.log(values) 
+  };
+
   render() {
-    console.log(this.props)
     if (this.props.modalTaskUpdate.state === 'close') {
       return null;
     }
     const { taskId } = this.props.modalTaskUpdate;
     const task = this.props.tasks.filter(({ id }) => id === taskId)[0];
-    console.log(task)
     return(
       <div className="modal" style={{display: 'block'}} tabIndex="-1" role="dialog">
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Modal title</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <button onClick={this.closeModal} type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div className="modal-body">
-              <p>{ task.text }</p>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-primary">Save changes</button>
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+            <form onSubmit={this.props.handleSubmit(this.saveUpdatedTask(taskId))}>
+              <div className="modal-body">
+                <p>{ task.text }</p>
+              </div>
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">Save changes</button>
+                <button onClick={this.closeModal} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>        
     );
   }
 }
+
+export default reduxForm({
+  form: 'updtaeTask',
+})(ModalTaskUpdate);
